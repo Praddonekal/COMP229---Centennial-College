aefee032f6781aa3687401e47b309f0b922bef07
Pradyumna <praddonekal@gmail.com>
2021-05-26 20:46:50 +0530

    Restructured the code

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-var app = require('../app');
+var app = require('./app');
 var debug = require('debug')('comp229---centennial-college:server');
 var http = require('http');
 
