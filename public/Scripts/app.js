ec4c4513cae4b25d4bb1bfc7178d003d8aac998e
Pradyumna <praddonekal@gmail.com>
2021-05-31 18:21:34 +0530

    added comments to all files

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,3 +1,7 @@
+/* simple javascript to show application is running 
+Pradyumna Donekal
+301145218
+30/05/2020*/
 (function(){
     function start()
     {
--- routes/index.js
+++ routes/index.js
@@ -6,22 +6,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
+/* GET home page. */
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home',});
 });
 
+/* GET about page. */
 router.get('/about', function(req, res, next) {
   res.render('index', { title: 'About',});
 });
 
+/* GET projects page. */
 router.get('/projects', function(req, res, next) {
   res.render('index', { title: 'Projects',});
 });
 
+/* GET services page. */
 router.get('/services', function(req, res, next) {
   res.render('index', { title: 'Services',});
 });
 
+/* GET contact page. */
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact',});
 });
